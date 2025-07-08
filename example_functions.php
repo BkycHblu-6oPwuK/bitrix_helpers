67965fd5596e98ec8414ea26ac6b1104b0c3461c
Alexandr.Belotsitsko <sanya_belyy_0202@mail.ru>
2025-07-08 10:46:43 +0600

    add

--- example_functions.php
+++ example_functions.php
@@ -1,6 +1,12 @@
 <?php
 require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 
-\Bitrix\Main\Diag\Debug::dumpToFile(array('ID' => $id, 'fields'=>$fields ),"","111111_logs/1.txt"); 
+\Bitrix\Main\Diag\Debug::dumpToFile(['ID' => $id, 'fields' => $fields], "", "111111_logs/1.log");
+
+file_put_contents(
+    $_SERVER['DOCUMENT_ROOT'] . '/1.log',
+    print_r($data, true) . PHP_EOL,
+    FILE_APPEND
+);
 
 require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/epilog_after.php");
