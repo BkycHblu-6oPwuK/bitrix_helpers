c5222ce0d5bcc7bba45770a1f023019da8339eba
Alexandr Belotsitsko <sanyabelyy020@gmail.com>
2026-04-16 10:40:00 +0600

    small refactoring

--- README.md
+++ README.md
@@ -8,3 +8,7 @@
 инфоблоки под контент, видео, главную, баннер, вопросы и ответы. Базовая реализация.
 
 под контент и главную есть компоненты в local/components/main|content
+
+[restore.php](https://www.1c-bitrix.ru/download/files/scripts/restore.php)
+
+[bitrixsetup.php](https://www.1c-bitrix.ru/download/files/scripts/bitrixsetup.php)
--- example_functions.php
+++ example_functions.php
@@ -1,12 +1,12 @@
 <?php
 require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
-
-\Bitrix\Main\Diag\Debug::dumpToFile(['ID' => $id, 'fields' => $fields], "", "111111_logs/1.log");
+/*
+\Bitrix\Main\Diag\Debug::dumpToFile(['ID' => $id, 'fields' => $fields], "", "local/logs/1.log");
 
 file_put_contents(
-    $_SERVER['DOCUMENT_ROOT'] . '/1.log',
+    $_SERVER['DOCUMENT_ROOT'] . '/local/logs/1.log',
     print_r($data, true) . PHP_EOL,
     FILE_APPEND
 );
-
+*/
 require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/epilog_after.php");
